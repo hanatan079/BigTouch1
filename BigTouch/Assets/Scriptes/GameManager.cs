541c3ecb06fd4c06a4cde9b14a07ba7b56880e35
Hanatan <hanatan079@gmail.com>
2023-01-24 16:13:46 +0900

    生成されたボール格納用変数

--- BigTouch/Assets/Scriptes/GameManager.cs
+++ BigTouch/Assets/Scriptes/GameManager.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    //生成されたボール格納
+    GameObject activeBall;
+
     void Awake()
     {
         //オブジェクトを格納
         bigSizeBall = GameObject.FindObjectOfType<BigSizeBall>();
+        if(!activeBall)
+        {
+            activeBall = bigSizeBall.SpawnBall();
+        }
     }
+
     // Start is called before the first frame update
     void Start()
     {
