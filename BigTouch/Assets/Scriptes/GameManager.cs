ba495d5e9836202102103ac96569d45f65a1b134
Hanatan <hanatan079@gmail.com>
2023-01-24 16:09:43 +0900

    オブジェクトを格納

--- BigTouch/Assets/Scriptes/GameManager.cs
+++ BigTouch/Assets/Scriptes/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         //オブジェクトを格納
-        bigSizeBall = GameObject.FindObjectOfType<BigSizeBall>(;)
+        bigSizeBall = GameObject.FindObjectOfType<BigSizeBall>();
     }
     // Start is called before the first frame update
     void Start()
