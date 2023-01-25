fa72084eaf780fbfb392cb27e1d3c42346442cc4
Hanatan <hanatan079@gmail.com>
2023-01-25 20:56:16 +0900

    大きいボールランダムな場所へ生成する修正

--- BigTouch/Assets/Scriptes/BigSizeBall.cs
+++ BigTouch/Assets/Scriptes/BigSizeBall.cs
@@ -8,6 +8,14 @@ public class BigSizeBall : MonoBehaviour
     //生成するBIGブロックを格納
     public GameObject[] bigBalls;
 
+    //ポシション格納用
+    private float ballPosX, ballPosY;
+
+    //新しいポジション生成格納用
+    Vector3 ballPos;
+
+
+
     //ランダムなブロックを1つ選ぶ 
     GameObject GetBigBall()
     {
@@ -26,7 +34,11 @@ public class BigSizeBall : MonoBehaviour
     //選ばれたブロックを生成する関数
     public GameObject SpawnBall()
     {
-        GameObject bigBall = Instantiate(GetBigBall(), transform.position, Quaternion.identity);
+                //生成場所をランダムで設定
+            ballPosX = Random.Range(-8.5f, 8.5f);
+            ballPosY = Random.Range(-4.5f, 4.5f);
+
+        GameObject bigBall = Instantiate(GetBigBall(), new Vector3(ballPosX, ballPosY, 0), Quaternion.identity);
         if(bigBall)
         {
             return bigBall;
