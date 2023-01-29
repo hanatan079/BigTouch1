de39a1ee3c294306f444801cb771d01e8da069a3
Hanatan <hanatan079@gmail.com>
2023-01-29 21:39:35 +0900

    修正

--- BigTouch/Assets/Scriptes/TwoSizeBall.cs
+++ BigTouch/Assets/Scriptes/TwoSizeBall.cs
@@ -6,29 +6,8 @@ public class TwoSizeBall : MonoBehaviour
 {
 
     //生成する中小ボールを格納
-    public GameObject[] bigBalls;
+    public GameObject[] twoSizeBalls;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-
-
-/*
-{
-
-    //大きいボール以外の配列格納
-    [SerializeField]
-    private GameObject[] twoSizeBalls;
 
     //ボールpos格納
     private float ballPosX, ballPosY;
@@ -36,15 +15,12 @@ public class TwoSizeBall : MonoBehaviour
     //生成したボールオブジェクト格納用
     private GameObject newTwoSizeBall;
 
-        //新しいポジション生成格納用
-    Vector3 ballPos;
-
     //生成されたボール格納(中小)
     GameObject activeTwoSizeBall;
 
 
 
-    
+
     void Awake()
     {
         //ボールのオブジェクトを格納
@@ -52,7 +28,16 @@ public class TwoSizeBall : MonoBehaviour
         {
             GetTwoSizeBall();
         }
+    }
 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
     }
 
 
@@ -60,17 +45,12 @@ public class TwoSizeBall : MonoBehaviour
     //配列から取り出す
     void GetTwoSizeBall()
     {
-        
-            //生成場所をランダムで設定
-            ballPosX = Random.Range(-8.5f, 8.5f);
-            ballPosY = Random.Range(-4.5f, 4.5f);
+        //生成場所をランダムで設定
+        ballPosX = Random.Range(-8.5f, 8.5f);
+        ballPosY = Random.Range(-4.5f, 4.5f);
 
-            newTwoSizeBall = Instantiate(twoSizeBalls,new Vector3(ballPosX, ballPosY, 0), Quaternion.identity);
-            //親オブジェクトを変える
-            newTwoSizeBall.transform.SetParent(transform);
-            
-
+        newTwoSizeBall = Instantiate(twoSizeBalls,new Vector3(ballPosX, ballPosY, 0), Quaternion.identity);
+        //親オブジェクトを変える
+        newTwoSizeBall.transform.SetParent(transform);
     }
 }
-
-*/
